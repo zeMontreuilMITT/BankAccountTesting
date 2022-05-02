d925db60f72861107052b4b8e9a4b8390577f4ec
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-05-02 13:07:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿static void Main(string[] args)
+{
+
+}
+
+public class Account
+{
+    private double _balance;
+    public double balance
+    {
+        get { return _balance; }
+        set { _balance = value; }
+    }
+
+    public double Withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            throw new ArgumentException("Error: insufficient funds for withdrawal.");
+        } else if (amount <= 0 || amount == null)
+        {
+            throw new ArgumentException("Withdrawals must be greater than $0");
+        }
+
+        balance = balance - amount;
+        return balance;
+    }
+
+    public double Deposit(double amount)
+    {
+        if(amount <= 0 || amount == null)
+        {
+            throw new ArgumentException("Deposits must be in amounts greater than $0");
+        }
+
+        balance += amount;
+        return balance;
+    }
+}
